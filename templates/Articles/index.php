801f02dffc0f9e6722685d462fbd9e46491ca784
May Meow <may@maymeow.com>
2022-10-26 21:51:20 +0200

    add bootstrap framework

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -6,41 +6,15 @@
 ?>
 <div class="articles index content">
     <?= $this->Html->link(__('New Article'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Articles') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('user_id') ?></th>
-                    <th><?= $this->Paginator->sort('title') ?></th>
-                    <th><?= $this->Paginator->sort('slug') ?></th>
-                    <th><?= $this->Paginator->sort('published') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($articles as $article): ?>
-                <tr>
-                    <td><?= $this->Number->format($article->id) ?></td>
-                    <td><?= $article->has('user') ? $this->Html->link($article->user->email, ['controller' => 'Users', 'action' => 'view', $article->user->id]) : '' ?></td>
-                    <td><?= h($article->title) ?></td>
-                    <td><?= h($article->slug) ?></td>
-                    <td><?= h($article->published) ?></td>
-                    <td><?= h($article->created) ?></td>
-                    <td><?= h($article->modified) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $article->slug]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $article->slug]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $article->slug], ['confirm' => __('Are you sure you want to delete article with slug {0}?', $article->slug)]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
+    <?php foreach ($articles as $article): ?>
+        <div id="Article-<?= $this->Number->format($article->id) ?>" class="mb-3">
+            <a href="<?= $this->Url->build(['action' => 'view', $article->slug]) ?>">
+                <h2><?= h($article->title) ?></h2>
+            </a>
+            <div><?= h($article->created) ?></div>
+        </div>
+    <?php endforeach; ?>
+
     <div class="paginator">
         <ul class="pagination">
             <?= $this->Paginator->first('<< ' . __('first')) ?>
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -14,7 +14,7 @@
  * @var \MeowBlog\View\AppView $this
  */
 
-$cakeDescription = 'CakePHP: the rapid development php framework';
+$cakeDescription = 'Meowblog';
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,29 +29,34 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 
-    <?= $this->Html->css(['theme']) ?>
+    <?= $this->Html->css(['bootstrap.min']) ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
+    <nav class="top-nav mb-5">
+        <div class="container mx-auto">
+            <div class="top-nav-title">
+                <a href="<?= $this->Url->build('/') ?>">
+                    <h1>Meowblog</h1>
+                </a>
+            </div>
         </div>
     </nav>
     <main class="main">
-        <div class="container">
+        <div class="container mx-auto">
             <?= $this->Flash->render() ?>
             <?= $this->fetch('content') ?>
         </div>
     </main>
     <footer>
+        <div class="container">
+            Powered by <a href="https://meowblog.app/" target="_blank">Meowblog</a>
+            v0.0.1
+            <a href="https://github.com/MayMeow/meowblog/" target="_blank">GitHub</a>
+        </div>
     </footer>
 </body>
 </html>
