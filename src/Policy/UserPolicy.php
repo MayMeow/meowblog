319420b31e80dc850778a125bf1156356125c7c1
May <may@maymeow.com>
2023-02-04 17:30:46 +0100

    Add user poliocy

---
+++ src/Policy/UserPolicy.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace MeowBlog\Policy;
+
+use Authorization\IdentityInterface;
+use MeowBlog\Model\Entity\User;
+
+/**
+ * User policy
+ */
+class UserPolicy
+{
+    /**
+     * Check if $user can add User
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \MeowBlog\Model\Entity\User $resource
+     * @return bool
+     */
+    public function canAdd(IdentityInterface $user, User $resource)
+    {
+        return true;
+    }
+
+    /**
+     * Check if $user can edit User
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \MeowBlog\Model\Entity\User $resource
+     * @return bool
+     */
+    public function canEdit(IdentityInterface $user, User $resource)
+    {
+        return true;
+    }
+
+    /**
+     * Check if $user can delete User
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \MeowBlog\Model\Entity\User $resource
+     * @return bool
+     */
+    public function canDelete(IdentityInterface $user, User $resource)
+    {
+        return true;
+    }
+
+    /**
+     * Check if $user can view User
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \MeowBlog\Model\Entity\User $resource
+     * @return bool
+     */
+    public function canView(IdentityInterface $user, User $resource)
+    {
+        return true;
+    }
+}
