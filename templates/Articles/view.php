9f9ebfc719d1de40a300736edc6f271ecc32cbfd
May <may@maymeow.com>
2023-05-07 13:17:35 +0200

    show tags on article view

--- templates/Articles/view.php
+++ templates/Articles/view.php
@@ -6,10 +6,21 @@
 ?>
 <div class="headings">
     <h2><?= $article->title ?></h2>
-    <h3>at <?= $article->created ?>
+    <h3>at <?= $article->created ?></h3>
 </div>
 <div id="article">
     <div>
         <?= $this->Markdown->parse($article->body); ?>
     </div>
+    <?php if ($article->tags) : ?>
+        <div class="tags">
+            <nav>
+                <ul>
+                    <?php foreach ($article->tags as $tag) : ?>
+                        <li><?= $this->Html->link($tag->title, ['controller' => 'Tags', 'action' => 'view', $tag->id], ['role' => 'button']) ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </nav>
+        </div>
+    <?php endif; ?>
 </div>
